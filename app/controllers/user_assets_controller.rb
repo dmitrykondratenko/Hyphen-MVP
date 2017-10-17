6c638aaec73abf1a258a727a70f67d7d98281f77
Mike Calhoun <mikedcalhoun@gmail.com>
2017-10-17 13:03:29 -0700

    x

--- app/controllers/user_assets_controller.rb
+++ app/controllers/user_assets_controller.rb
@@ -12,7 +12,7 @@ class UserAssetsController < ApplicationController
     response = RestClient.post 'http://127.0.0.1:5001/api/v0/add', :myfile => file
     ipfs_hash = JSON.parse(response)["Hash"]
     contract = Ethereum::Contract.create(file: "/home/deploy/apps/hyphen/current/app/contracts/HyphenTwo.sol", address: "0x2BDBF71338B2E6BaEE43CcF82Fe735909081dF40")
-    result = contract.transact_and_wait.register("name", current_user.email, "ipfshash", ipfs_hash, "created", Time.now.to_s, client: client)
+    result = contract.transact_and_wait.register("name", current_user.email, "ipfshash", ipfs_hash, "created", Time.now.to_s)
     new_asset = UserAsset.create(user_id: current_user.id, ipfs_hash: ipfs_hash, ethereum_transaction_id: result.id)
     redirect_to action: "index"  end
 
